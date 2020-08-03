aa7bb14aa31c7ea9031b36e5a4cddb0d36885282
Donald A. Lobo <donald.lobo@gmail.com>
2020-08-02 21:07:09 -0700

    lets try out vapor

---
+++ lib/glific/config.ex
@@ -0,0 +1,19 @@
+defmodule Glific.Config do
+  use Vapor.Planner
+
+  dotenv()
+
+  config :db, env([
+    {:url, "DB_URL"},
+    {:name, "DB_NAME"},
+    {:pool_size, "DB_POOL_SIZE", default: 10, map: &String.to_integer/1},
+  ])
+
+  config :web env([
+    {:port, "PORT", default: 4000, map: &String.to_integer/1}
+  ])
+
+  config :frontend env([
+    {:}
+  ])
+end
